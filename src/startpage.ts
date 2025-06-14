e0c50c057c34f6358cd3a5198fc69cffec384538
ChristianCav <christiancav55@gmail.com>
2025-06-13 21:07:36 -0400

    startpage options

--- src/startpage.ts
+++ src/startpage.ts
@@ -12,12 +12,36 @@ player_list.push("Martinelli")
 player_list.push("Zidane");
 console.log(player_list.sort(player_list.alphaAscendingSort));
 
+// battle button
+const BATTLEBUTTON: HTMLButtonElement = document.getElementById("battle-button") as HTMLButtonElement;
+// view players button
+const VIEWPLAYERSBUTTON: HTMLButtonElement = document.getElementById("view-players-button") as HTMLButtonElement;
+
+console.log((document.getElementById("battle-options")as HTMLElement).hidden)
+BATTLEBUTTON.addEventListener("click",()=>{
+    hideStartPage();
+    displayUserOptions("battle");
+});
+
+VIEWPLAYERSBUTTON.addEventListener("click", ()=>{
+    hideStartPage();
+    displayUserOptions("players");
+});
+
+/**
+ * Hides all elements from the starting page
+ */
+function hideStartPage(): void{
+    BATTLEBUTTON.hidden = true;
+    VIEWPLAYERSBUTTON.hidden = true;
+    (document.getElementById("title") as HTMLElement).hidden = true;
+}
+
 const SEARCHBAR = document.getElementById("search-bar") as HTMLInputElement;
 const SEARCHBUTTON = document.getElementById("search-button") as HTMLButtonElement;
 const DISPLAY = document.getElementById("display") as HTMLParagraphElement;
-SEARCHBUTTON.addEventListener("click", ()=>{
-    handleSearch(SEARCHBAR.value);
-})
+
+
 function handleSearch(search: string): void{
     SEARCHBAR.value = "";
     let result: number[] = player_list.binarySearch(search, player_list.getSortedData(), player_list.alphaAscendingSearch);
@@ -30,3 +54,12 @@ function handleSearch(search: string): void{
     DISPLAY.innerText = html;
 }
 
+function displayUserOptions(page: string): void{
+    if(page === "battle"){
+        (document.getElementById("battle-options")as HTMLElement).style.display = "flex";
+        (document.getElementById("selection-title")as HTMLElement).hidden = false;
+    }
+    else if(page === "players"){
+        console.log("Display players")
+    }
+}
